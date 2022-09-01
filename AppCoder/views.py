f2fc7589f13c3b54d74770ffdea077e5aa173561
= <=>
2022-09-01 15:54:00 -0500

    Haciendo modificaciones para mostrar la información en las plantillas. EGP.

--- AppCoder/views.py
+++ AppCoder/views.py
@@ -5,16 +5,30 @@ from django.http import HttpResponse
 # Create your views here.
 
 def familia(request):
-    familiar1 = Familia(nombre="Diego Said", edad=37, fechaNacimiento="1985-02-15")
-    familiar2 = Familia(nombre="Gerardo Said", edad=35, fechaNacimiento="14/10/1986")
-    familiar3 = Familia(nombre="Franco Said", edad=31, fechaNacimiento="31/05/1990")
+    familiar_1 = Familia(nombre="Diego Said", edad=37, fecha_nacimiento="1985-02-15")
+    familiar_2 = Familia(nombre="Gerardo Said", edad=35, fecha_nacimiento="1986-10-14")
+    familiar_3 = Familia(nombre="Franco Said", edad=31, fecha_nacimiento="1990-05-31")
 
-    familiar1.save()
-    familiar2.save()
-    familiar3.save()
+    familiar_1.save()
+    familiar_2.save()
+    familiar_3.save()
 
-    texto1=f"nombre: {familiar1.nombre}, edad: {familiar1.edad} y fecha de nacimiento: {familiar1.fechaNacimiento}"
-    texto2=f"nombre: {familiar2.nombre}, edad: {familiar2.edad} y fecha de nacimiento: {familiar2.fechaNacimiento}"
-    texto3=f"nombre: {familiar3.nombre}, edad: {familiar3.edad} y fecha de nacimiento: {familiar3.fechaNacimiento}"
+    familiares = {
+        familiar_1:{
+            'nombre': familiar_1.nombre,
+            'edad': familiar_1.edad,
+            'fecha_nacimiento': familiar_1.fecha_nacimiento
+        },
+        familiar_2:{
+            'nombre': familiar_2.nombre,
+            'edad': familiar_2.edad,
+            'fecha_nacimiento': familiar_2.fecha_nacimiento
+        },
+        familiar_3:{
+            'nombre': familiar_3.nombre,
+            'edad': familiar_3.edad,
+            'fecha_nacimiento': familiar_3.fecha_nacimiento
+        }
+    }
 
-    return render(request,"AppCoder/familia.html")
+    return render(request,"AppCoder/familia.html", {'familiares':familiares})
